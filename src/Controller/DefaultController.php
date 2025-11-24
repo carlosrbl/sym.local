dc92976a335073c35dbbdd4b2a886636233a0046
Carlos Rodrigo Beltrá <carrodbel3@alu.edu.gva.es>
2025-11-24 10:09:56 +0100

    subida proyecto sinfony

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{
+    public function index()
+    {
+        return $this->render('prueba.html.twig');
+    }
+}
