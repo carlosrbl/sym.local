dbf22450b3ac3d5966dd146ede780dcd20eeeda2
Carlos Rodrigo Beltrá <carrodbel3@alu.edu.gva.es>
2025-11-26 10:05:21 +0100

    Proyecto Symfony 2.1: Vistas y rutas

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -10,11 +10,27 @@ class DefaultController extends AbstractController
     {
         $nombre = 'Carlos';
         $saludo = 'Buenos días';
-        $nombres = [ 'Ana','Enrique','Laura','Pablo' ];
+        $nombres = ['Ana', 'Enrique', 'Laura', 'Pablo'];
         return $this->render('prueba.html.twig', [
             'nombre' => $nombre,
             'saludo' => $saludo,
             'nombres' => $nombres
+        ]);
+    }
+    public function index1()
+    {
+        return $this->render('prueba1.html.twig');
+    }
+    public function index2()
+    {
+        $nombre = 'Juan';
+        $saludo = 'Buenos días a todos';
+        $nombres = ['Ana', 'Enrique', 'Laura', 'Pablo'];
+        return $this->render('prueba2.html.twig', [
+            'nombre' => $nombre,
+            'saludo' => $saludo,
+            'nombres' => $nombres,
+            'fecha' => new \DateTime()
         ]);
     }
 }
